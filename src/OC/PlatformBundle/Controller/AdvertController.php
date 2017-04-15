c73f61ed26705f93538dfbf4968567650f39a7c3
Relwyn <relwyn@relwyn>
2017-04-15 16:16:22 +0200

    Initial commit

---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,19 @@
+<?php
+
+// src/OC/PlatformBundle/Controller/AdvertController.php
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class AdvertController extends Controller
+{
+  public function menuAction($limit)
+  {
+    // On fixe en dur une liste ici, bien entendu par la suite on la récupérera depuis la BDD !
+
+    return $this->render('OCPlatformBundle:Advert:menu.html.twig');
+  }
+}
